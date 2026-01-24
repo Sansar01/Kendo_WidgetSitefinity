b4cacc1496a36912967cddcf5812ce19bad9d614
Sansar Tiwari <st9889477@gmail.com>
2026-01-24 19:39:25 +0530

    kendo chart

---
+++ Mvc/Controllers/KendoChartApiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+
+namespace Kendo_WidgetSitefinity.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "KendoChart", SectionName = "Kendos", Title = "Kendo Charts")]
+    public class KendoChartApiController : Controller
+    {
+        // GET: KendoChartApi
+        public ActionResult Index()
+        {
+            return View("Default");
+        }
+    }
+}
---
+++ Mvc/Scripts/chart.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    $(".options").bind("change", refresh);
+    $(document).bind("kendo:skinChange", updateTheme);
+});
+
+function refresh() {
+    var chart = $("#chart").data("kendoChart"),
+        series = chart.options.series,
+        type = $("input[name=seriesType]:checked").val(),
+        stack = $("#stack").prop("checked");
+
+    for (var i = 0, length = series.length; i < length; i++) {
+        series[i].stack = stack;
+        series[i].type = type;
+    };
+
+    chart.refresh();
+}
+
+function updateTheme() {
+    $("#chart").getKendoChart().setOptions({ theme: kendoTheme });
+}
